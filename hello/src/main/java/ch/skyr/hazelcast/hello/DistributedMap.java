270ce43aac125f42102df0773dc7a4c83a7da1c6
Tom Plüss <tom.pluess@skyr.ch>
2014-05-29 17:44:19 +0200

    Hazelcast Config that runs locally.

--- hello/src/main/java/ch/skyr/hazelcast/hello/DistributedMap.java
+++ hello/src/main/java/ch/skyr/hazelcast/hello/DistributedMap.java
@@ -1,20 +1,50 @@
 package ch.skyr.hazelcast.hello;
 
+import java.util.concurrent.ConcurrentMap;
+
 import com.hazelcast.config.Config;
+import com.hazelcast.config.GroupConfig;
 import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
- 
-import java.util.concurrent.ConcurrentMap;
- 
+
 public class DistributedMap {
-    public static void main(String[] args) {
-        Config config = new Config();
-        HazelcastInstance h = Hazelcast.newHazelcastInstance(config);
-        ConcurrentMap<String, String> map = h.getMap("my-distributed-map");
+    private static final String MAP_NAME = "my-distributed-map";
+
+    public static void main(final String[] args) throws InterruptedException {
+
+        final String instanceName = "testInstance";
+        final String groupName = "groupName";
+
+        final Config config = new Config();
+        // config.setInstanceName(instanceName );
+        config.setGroupConfig(new GroupConfig(groupName, "lol"));
+        config.getNetworkConfig().getJoin().getMulticastConfig().setEnabled(false);
+        config.getNetworkConfig().getJoin().getTcpIpConfig().setEnabled(true);
+        config.getNetworkConfig().getJoin().getTcpIpConfig().addMember("127.0.0.1");
+        // config.getNetworkConfig().getInterfaces().clear();
+        // config.getNetworkConfig().getInterfaces().addInterface("127.0.0.1");
+        // config.getNetworkConfig().getInterfaces().setEnabled(true);
+
+        final HazelcastInstance h = Hazelcast.newHazelcastInstance(config);
+        final ConcurrentMap<String, String> map = h.getMap(MAP_NAME);
         map.put("key", "value");
         map.get("key");
-        //Concurrent Map methods
+        // Concurrent Map methods
         map.putIfAbsent("somekey", "somevalue");
         map.replace("key", "value", "newvalue");
+
+        //
+        map.put("ts-" + System.currentTimeMillis(), "bla");
+
+        while (true) {
+            printMap(h);
+            Thread.sleep(3000);
+        }
+    }
+
+    private static void printMap(final HazelcastInstance h) {
+        final ConcurrentMap<String, String> map = h.getMap(MAP_NAME);
+        System.out.println("map: " + map);
+        System.out.println("keys: " + map.keySet());
     }
 }
