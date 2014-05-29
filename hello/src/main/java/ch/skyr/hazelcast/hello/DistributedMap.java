21635d3a7cbfa43164f6a8f472ae859af84de463
Tom Plüss <tom.pluess@skyr.ch>
2014-05-29 14:59:05 +0200

    implement and build example class DistributedMap.

---
+++ hello/src/main/java/ch/skyr/hazelcast/hello/DistributedMap.java
@@ -0,0 +1,20 @@
+package ch.skyr.hazelcast.hello;
+
+import com.hazelcast.config.Config;
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
+ 
+import java.util.concurrent.ConcurrentMap;
+ 
+public class DistributedMap {
+    public static void main(String[] args) {
+        Config config = new Config();
+        HazelcastInstance h = Hazelcast.newHazelcastInstance(config);
+        ConcurrentMap<String, String> map = h.getMap("my-distributed-map");
+        map.put("key", "value");
+        map.get("key");
+        //Concurrent Map methods
+        map.putIfAbsent("somekey", "somevalue");
+        map.replace("key", "value", "newvalue");
+    }
+}
